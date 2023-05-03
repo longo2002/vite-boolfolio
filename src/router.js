83765c3360dfa07ca7c843f365092cfa0b0b3c7a
longo2002 <lombardo.fra2002@gmail.com>
2023-05-03 14:02:28 +0200

    attempt 1

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from './views/Home.vue';
-import Portfolio from './views/Portfolio.vue';
-import About from './views/About.vue';
+import Home from './components/Home.vue';
+import Portfolio from './components/Portfolio.vue';
+import About from './components/About.vue';
 
 const routes = [
     {
