3231f10aa449c2116bc3758c39c8983127f5ee06
longo2002 <lombardo.fra2002@gmail.com>
2023-05-03 13:43:38 +0200

    attempt 1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app">
+    <h1>Progetti</h1>
+    <div v-if="loading">Caricamento...</div>
+    <div v-else>
+      <div v-for="project in projects" :key="project.id">
+        <h2>{{ project.name }}</h2>
+        <p>{{ project.description }}</p>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      projects: [],
+      loading: true,
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get("http://localhost:8000/api/projects");
+      this.projects = response.data;
+      this.loading = false;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+</script>
